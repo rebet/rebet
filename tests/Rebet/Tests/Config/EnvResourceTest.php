80dee916e307754e12360e41eee7aa64510aec45
Junichi Shibata <rain.noise@gmail.com>
2018-09-15 23:27:38 +0900

    Update EnvResource tests

--- tests/Rebet/Tests/Config/EnvResourceTest.php
+++ tests/Rebet/Tests/Config/EnvResourceTest.php
@@ -44,23 +44,25 @@ return [
 ];
 EOS
                     ,
-                    'test.ini' => <<<EOS
-[a]
-int: 1
-string: a
-[b]
-bool: on
-string: b
-EOS
-                    ,
                     'test_unittest.ini' => <<<EOS
 [a]
-int: 2
-new: NEW
+int = 2
+string = a
 [b]
-string: B
-[c]
-new: C
+string = b
+EOS
+                    ,
+                    'test.json' => <<<EOS
+{
+    "int": 1,
+    "string": "a",
+    "array": [1 ,2 , 3],
+    "map": {
+        "int": 1,
+        "string": "a",
+        "array": [1 ,2 , 3]
+    }
+}
 EOS
                     ,
                 ],
@@ -85,13 +87,42 @@ EOS
             ],
             EnvResource::load('vfs://root/resource', 'test')
         );
+        
+        $this->assertSame(
+            [
+                'a' => [
+                    'int' => 2,
+                    'string' => 'a',
+                ],
+                'b' => [
+                    'string' => 'b',
+                ]
+            ],
+            EnvResource::load('vfs://root/resource', 'test', 'ini')
+        );
+        
+        $this->assertSame(
+            [
+                'int' => 1,
+                'string' => 'a',
+                'array' => [1 ,2 , 3],
+                'map' => [
+                    'int' => 1,
+                    'string' => 'a',
+                    'array' => [1 ,2 , 3],
+                ],
+            ],
+            EnvResource::load('vfs://root/resource', 'test', 'json')
+        );
     }
 
-    // /**
-    //  * @expectedException \LogicException
-    //  * @expectedExceptionMessage Invalid path format: c:/invalid/../../path
-    //  */
-    // public function test_xxxxx_invalid() {
-    // }
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Resource test txt not found in vfs://root/resource.
+     */
+    public function test_xxxxx_invalid() {
+        EnvResource::load('vfs://root/resource', 'test', 'txt');
+        $this->fail("Never execute.");
+    }
 
 }
