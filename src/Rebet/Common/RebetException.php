62e00a7a5def64fa36fe6af7e40eabcab6606e2e
Junichi Shibata <rain.noise@gmail.com>
2018-12-13 00:36:12 +0900

    Add RebetException

---
+++ src/Rebet/Common/RebetException.php
@@ -0,0 +1,19 @@
+<?php
+namespace Rebet\Common;
+
+/**
+ * Rebet Exception Interface
+ *
+ * @package   Rebet
+ * @author    github.com/rain-noise
+ * @copyright Copyright (c) 2018 github.com/rain-noise
+ * @license   MIT License https://github.com/rebet/rebet/blob/master/LICENSE
+ */
+interface RebetException
+{
+    public static function by(string $message) : self ;
+    
+    public function caused(\Throwable $previous) : self ;
+    
+    public function code(int $code) : self ;
+}
