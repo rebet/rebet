5cd1bae2befa1342e5f75851addfb018e21de04a
Junichi Shibata <rain.noise@gmail.com>
2018-12-12 23:13:27 +0900

    Update

--- src/Rebet/Auth/AuthenticateException.php
+++ src/Rebet/Auth/AuthenticateException.php
@@ -1,6 +1,10 @@
 <?php
 namespace Rebet\Auth;
 
+use Rebet\Http\ProblemRespondable;
+use Rebet\Http\Responder;
+use Rebet\Http\Response\ProblemResponse;
+
 /**
  * Authenticate Exception Class
  *
@@ -9,10 +13,18 @@ namespace Rebet\Auth;
  * @copyright Copyright (c) 2018 github.com/rain-noise
  * @license   MIT License https://github.com/rebet/rebet/blob/master/LICENSE
  */
-class AuthenticateException extends \Exception
+class AuthenticateException extends \RuntimeException implements ProblemRespondable
 {
     public function __construct($message, $code = null, $previous = null)
     {
         parent::__construct($message, $code, $previous);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function problem() : ProblemResponse
+    {
+        return Responder::problem(403)->detail($this->getMessage());
     }
 }
