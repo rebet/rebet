1428faacb1e8a2f9c57e23104769188c7b9a89a3
Junichi Shibata <rain.noise@gmail.com>
2018-12-13 19:06:24 +0900

    update

--- src/Rebet/View/Exception/ViewRenderFailedException.php
+++ src/Rebet/View/Exception/ViewRenderFailedException.php
@@ -1,7 +1,7 @@
 <?php
 namespace Rebet\View\Exception;
 
-use Rebet\Common\Exception\RuntimeException;
+use Rebet\Common\Exception\LogicException;
 
 /**
  * View Render Failed Exception Class
@@ -13,7 +13,7 @@ use Rebet\Common\Exception\RuntimeException;
  * @copyright Copyright (c) 2018 github.com/rain-noise
  * @license   MIT License https://github.com/rebet/rebet/blob/master/LICENSE
  */
-class ViewRenderFailedException extends RuntimeException
+class ViewRenderFailedException extends LogicException
 {
     public function __construct(string $message, ?\Throwable $previous = null, int $code = 500)
     {
