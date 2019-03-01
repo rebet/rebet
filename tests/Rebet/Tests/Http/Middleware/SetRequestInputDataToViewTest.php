9a3e32ae83a813e94f7adc2e9422da052b01d71a
Junichi Shibata <rain.noise@gmail.com>
2019-03-01 17:12:19 +0900

    Add SetRequestInputDataToViewTest

---
+++ tests/Rebet/Tests/Http/Middleware/SetRequestInputDataToViewTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Rebet\Tests\Http\Middleware;
+
+use Rebet\Http\Middleware\SetRequestInputDataToView;
+use Rebet\Http\Responder;
+use Rebet\Http\Response\BasicResponse;
+use Rebet\Stream\Stream;
+use Rebet\Tests\RebetTestCase;
+use Rebet\View\View;
+
+class SetRequestInputDataToViewTest extends RebetTestCase
+{
+    public function test___construct()
+    {
+        $this->assertInstanceOf(SetRequestInputDataToView::class, new SetRequestInputDataToView());
+    }
+
+    public function test_handle()
+    {
+        $middleware  = new SetRequestInputDataToView();
+        $destination = function ($request) { return Responder::toResponse('OK'); };
+
+        $request = $this->createRequestMock('/');
+        $request->query->set('query', 'Q');
+        $request->request->set('request', 'R');
+        $response = $middleware->handle($request, $destination);
+        $this->assertInstanceOf(BasicResponse::class, $response);
+        $this->assertSame('OK', $response->getContent());
+        $this->assertInstanceOf(Stream::class, View::shared('input'));
+        $this->assertSame([
+            'request' => 'R',
+            'query'   => 'Q',
+        ], View::shared('input')->return());
+    }
+}
