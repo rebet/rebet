47c03ae72aa9cb1899e3b888f19b0ddb4f3c8abc
Junichi Shibata <rain.noise@gmail.com>
2019-03-01 16:58:06 +0900

    Add RestoreInheritDataTest

---
+++ tests/Rebet/Tests/Http/Middleware/RestoreInheritDataTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Rebet\Tests\Http\Middleware;
+
+use Rebet\Http\Middleware\RestoreInheritData;
+use Rebet\Http\Responder;
+use Rebet\Http\Response\BasicResponse;
+use Rebet\Tests\RebetTestCase;
+use Rebet\View\View;
+
+class RestoreInheritDataTest extends RebetTestCase
+{
+    public function test___construct()
+    {
+        $this->assertInstanceOf(RestoreInheritData::class, new RestoreInheritData());
+    }
+
+    public function test_handle()
+    {
+        $middleware  = new RestoreInheritData();
+        $destination = function ($request) { return Responder::toResponse('OK'); };
+
+        $request = $this->createRequestMock('/');
+        $session = $request->session();
+        $session->saveInheritData('input', ['name' => 'Name']);
+        $session->saveInheritData('errors', ['name' => ['Invalid']]);
+        $this->assertSame([], $request->input());
+        $this->assertSame(null, View::shared('errors'));
+        $response = $middleware->handle($request, $destination);
+        $this->assertInstanceOf(BasicResponse::class, $response);
+        $this->assertSame('OK', $response->getContent());
+        $this->assertSame(['name' => 'Name'], $request->input());
+        $this->assertSame(['name' => ['Invalid']], View::shared('errors'));
+    }
+}
