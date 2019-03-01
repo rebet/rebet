f766816e5b3ed41db1f331dc904296f2faa48a33
Junichi Shibata <rain.noise@gmail.com>
2019-03-01 17:23:07 +0900

    Add StartSessionTest

---
+++ tests/Rebet/Tests/Http/Middleware/StartSessionTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Rebet\Tests\Http\Middleware;
+
+use Rebet\Http\Middleware\StartSession;
+use Rebet\Http\Request;
+use Rebet\Http\Responder;
+use Rebet\Http\Response\BasicResponse;
+use Rebet\Http\Session\Session;
+use Rebet\Tests\RebetTestCase;
+
+class StartSessionTest extends RebetTestCase
+{
+    public function test___construct()
+    {
+        $this->assertInstanceOf(StartSession::class, new StartSession());
+    }
+
+    public function test_handle()
+    {
+        $middleware  = new StartSession();
+        $destination = function ($request) { return Responder::toResponse('OK'); };
+
+        $request = Request::create('/');
+        try {
+            $request->session();
+            $this->fail('Never execute.');
+        } catch (\BadMethodCallException $e) {
+            $this->assertSame('Session has not been set', $e->getMessage());
+        }
+        $response = $middleware->handle($request, $destination);
+        $this->assertInstanceOf(BasicResponse::class, $response);
+        $this->assertSame('OK', $response->getContent());
+        $session = $request->session();
+        $this->assertInstanceOf(Session::class, $session);
+    }
+}
